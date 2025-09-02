c7766415e505d16684fe571f1d56ba5928d648a0
rishwithgomas <rishwithgomas@gmail.com>
2025-09-02 10:30:33 +0530

    a temparature converter

--- tempconverter.py
+++ tempconverter.py
@@ -2,7 +2,7 @@ temparature = (float(input("Enter temperature: ")))
 scale = input('Enter scale factor "C"/"F"/"K" : ') .strip() .upper()
 
 celsius_to_fahrenheit = ((temparature * 9) / 5) + 32
-celsius_to_kelvin = celsius_to_fahrenheit + 273.15
+celsius_to_kelvin = temparature + 273.15
 fahrenheit_to_celsius = (temparature - 32) * 5/9
 fahrenheit_to_kelvin = (temparature - 32) * 5/9 + 273.15
 kelvin_to_celsius = (temparature  - 273.15)
